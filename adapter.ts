d74a3f7d47b0e49236169cf0929b397923cfb2a5
Kaicho Sun <kaichaosuna@gmail.com>
2021-12-17 13:37:21 +0800

    fix endpoint

--- adapter.ts
+++ adapter.ts
@@ -2,6 +2,7 @@ import type { Address, Expression, ExpressionAdapter, PublicSharing, LanguageCon
 import type { IPFS } from "ipfs-core-types";
 import axios from "axios";
 import https from "https";
+import { GET_ENDPOINT, UPLOAD_ENDPOINT } from "./config";
 
 class SharedPerspectivePutAdapter implements PublicSharing {
   #agent: AgentService;
@@ -29,7 +30,7 @@ class SharedPerspectivePutAdapter implements PublicSharing {
       hash,
       content,
     };
-    const postResult = await axios.post("https://bi8fgdofma.execute-api.us-west-2.amazonaws.com/dev/serverlessSetup/upload", postData, { httpsAgent });
+    const postResult = await axios.post(UPLOAD_ENDPOINT, postData, { httpsAgent });
     if (postResult.status != 200) {
       console.error("Create neighbourhood error: ", postResult);
     }
@@ -55,7 +56,7 @@ export default class Adapter implements ExpressionAdapter {
     const httpsAgent = new https.Agent({
       rejectUnauthorized: false
     });
-    const getResult = await axios.get(`https://bi8fgdofma.execute-api.us-west-2.amazonaws.com/dev/flux-files/get?hash=${cid}`);
+    const getResult = await axios.get(`${GET_ENDPOINT}?hash=${cid}`);
     if (getResult.status != 200) {
       console.error("Create neighbourhood error: ", getResult);
     }
---
+++ config.ts
@@ -0,0 +1,2 @@
+export const UPLOAD_ENDPOINT = "https://bi8fgdofma.execute-api.us-west-2.amazonaws.com/dev/flux-files/upload"
+export const GET_ENDPOINT = "https://bi8fgdofma.execute-api.us-west-2.amazonaws.com/dev/flux-files/get"
