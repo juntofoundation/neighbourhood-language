d1ec7c9a493c7044fe2a1dfccedd5ddeef97b576
Kaichao Sun <kaichaosuna@gmail.com>
2021-12-20 22:59:21 +0800

    fix credentials with fake

--- config.ts
+++ config.ts
@@ -5,6 +5,10 @@ export const UPLOAD_ENDPOINT = "https://bi8fgdofma.execute-api.us-west-2.amazona
 export const s3 = new S3Client({
     region: "decentralized",
     endpoint: "http://init.so",
+    credentials: {
+        accessKeyId: "test key id",
+        secretAccessKey: "test secret key",
+    },
 });
 
 export const BUCKET_NAME = "flux";
